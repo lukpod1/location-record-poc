65f20b6b3f489d87d37889abb11ae51d2e2dcf0f
José Lucas Alves da Silva <lukasalvespod1@gmail.com>
2020-08-09 02:49:21 -0300

    added function fixes

--- src/createLocation.js
+++ src/createLocation.js
@@ -1,17 +1,17 @@
-import * as uuid from "uuid";
+// import * as uuid from "uuid";
 import dynamoDb from "../libs/dynamodb-lib";
 import handler from "../libs/handler-lib";
 
 export const main = handler(async (event, context) => {
 
+    let date = new Date();
+
     const params = {
         TableName: process.env.TABLE_NAME,
         Item: {
-            orderServiceId: event.body.orderServiceId,
-            locationId: uuid.v1(),
-            deliveryId: event.body.deliveryId,
-            position: event.body.position,
-            createdAt: Date.now()
+            orderServiceId: event.orderServiceId,
+            timestamp: date.toISOString(),
+            coordinates: event.coordinates
         }
     };
 
--- src/getLocation.js
+++ src/getLocation.js
@@ -2,15 +2,27 @@ import handler from "../libs/handler-lib";
 import dynamoDb from "../libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
+
+    let date = new Date();
+
     const params = {
         TableName: process.env.TABLE_NAME,
-        KeyConditionExpression: "orderServiceId = :orderServiceId",
+        ProjectionExpression:"coordinates",
+        KeyConditionExpression: "orderServiceId = :orderServiceId and #time between :past and :now",
+        ExpressionAttributeNames:{
+            "#time":"timestamp"
+        },
         ExpressionAttributeValues: {
-            ":orderServiceId": event.body.orderServiceId
+            ":orderServiceId": event.orderServiceId,
+            ":now": date.toISOString(),
+            ":past": event.contractDate
         }
     };
 
     const result = await dynamoDb.query(params);
+    const coordinates = result.Items.map(item => {
+        return item.coordinates;
+    });
 
-    return result.Items;
+    return coordinates;
 });
