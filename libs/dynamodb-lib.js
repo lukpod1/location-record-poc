c3b4bf391f8e7639610d8a838c16d22fb5094ce4
José Lucas Alves da Silva <lukasalvespod1@gmail.com>
2020-08-11 12:27:53 -0300

    correction

--- libs/dynamodb-lib.js
+++ libs/dynamodb-lib.js
@@ -6,6 +6,5 @@ export default {
     get: (params) => client.get(params).promise(),
     put: (params) => client.put(params).promise(),
     query: (params) => client.query(params).promise(),
-    update: (params) => client.update(params).promise(),
-    delete: (params) => client.delete(params).promise(),
+    scan: (params) => client.scan(params).promise(),
 };
--- src/createLocation.js
+++ src/createLocation.js
@@ -1,4 +1,4 @@
-// import * as uuid from "uuid";
+import * as uuid from "uuid";
 import dynamoDb from "../libs/dynamodb-lib";
 import handler from "../libs/handler-lib";
 
@@ -6,14 +6,19 @@ export const main = handler(async (event, context) => {
 
     let date = new Date();
 
+    let coordinates = event.coordinates[0];
+    coordinates.craetedAt = date.toISOString();
+
     const params = {
         TableName: process.env.TABLE_NAME,
         Item: {
-            orderServiceId: event.orderServiceId,
-            timestamp: date.toISOString(),
-            coordinates: event.coordinates
+            orderServiceId: "06b09eba-dbdc-11ea-87d0-0242ac130003",
+            deliveryId: uuid.v1(),
+            coordinates: coordinates
         }
     };
+
+    const locations = await dynamoDb.scan(params);
 
     await dynamoDb.put(params);
 
